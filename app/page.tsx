f25a26da0c053f466ac76751edfe229520462f6d
benjamin-mahmood <benmahmood@icloud.com>
2025-02-24 17:56:13 +0100

    added H1 component

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,11 @@ import Header from '@/components/header/Header'
 import Main from '@/components/main/Main'
 import Footer from '@/components/footer/Footer'
 import { ThemeToggle } from '@/components/ui/ThemeToggle'
-import { Button } from '@heroui/button'
 
 export default function Home() {
   return (
     <main>
       <ThemeToggle />
-      <Button color='primary'>Button</Button>
       <Header />
       <Main />
       <Footer />
--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,3 +1,9 @@
+import { H1 } from './H1'
+
 export default function Header() {
-  return <div>Header</div>
+  return (
+    <header>
+      <H1>Social-Media Post Checker</H1>
+    </header>
+  )
 }
